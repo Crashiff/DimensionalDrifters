d333587aef69ba4b25dc4e2bf13085d8f9f04729
Nati Shiff <natanel.shiff@gmail.com>
2023-07-07 19:06:49 +0300

    Teleportation fixed

--- Assets/TeleportCollider.cs
+++ Assets/TeleportCollider.cs
@@ -14,14 +14,14 @@ public class TeleportCollider : MonoBehaviour
         player2 = GameObject.FindGameObjectWithTag("Player2");
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerStay(Collider collision)
     {
-        if (collision.gameObject.tag == "Player1")
+        if (collision.gameObject.tag == "Player1" && (player2.transform.position.x > xDest + 1 || player2.transform.position.x < xDest - 1))
         {
             Debug.Log("Player1 coll");
             player1.transform.position = new Vector3(xDest, 0.54f, 8.02f);//(where you want to teleport)
         }
-        else if (collision.gameObject.tag == "Player2")
+        else if (collision.gameObject.tag == "Player2" && (player1.transform.position.x > xDest + 1 || player1.transform.position.x < xDest - 1))
         {
             Debug.Log("Player2 coll");
             player2.transform.position = new Vector3(xDest, 0.54f, 8.02f);//(where you want to teleport)
