8eac8d9f4a33214ac0262c9617d5c6a2e79dce66
Talbe <talbenzvi00@gmail.com>
2023-07-26 21:58:29 +0300

    WIP: work on fix collision with rigid body

---
+++ Assets/Scripts/2D_And_3D/ChangeLayerOnColiisionWithRigidbody.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class ChangeLayerOnCollisionWithRigidbody : MonoBehaviour
+{
+    //private GameObject buffer;
+    private GameObject prepareChild;
+    private GameObject colorChild;
+
+    void Start()
+    {
+        prepareChild = transform.Find("Prepare2D").gameObject;
+        colorChild = transform.Find("Color2D").gameObject;
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.name == "PlaneBuffer2D")
+        {
+            prepareChild.layer = LayerMask.NameToLayer("2DPlane");
+            colorChild.layer = LayerMask.NameToLayer("2DPlane");
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.name == "PlaneBuffer2D")
+        {
+            prepareChild.layer = LayerMask.NameToLayer("Default");
+            colorChild.layer = LayerMask.NameToLayer("Default");
+        }
+    }
+}
---
+++ Assets/emptyTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emptyTrigger : MonoBehaviour
+{
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+
+}
