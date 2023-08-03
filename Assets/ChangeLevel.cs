59a3a2d3916d9a15e36dec087161a7ebcd4e1000
Talbe <talbenzvi00@gmail.com>
2023-08-03 19:57:17 +0300

    finish change level script

--- Assets/ChangeLevel.cs
+++ Assets/ChangeLevel.cs
@@ -6,18 +6,38 @@ using UnityEngine.SceneManagement;
 public class ChangeLevel : MonoBehaviour
 {
 
-    static int counter = 0;
+    static int player1Counter = 0;
+    static int player2Counter = 0;
+    public string nextSceneName;
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.tag == "Player1" || collider.gameObject.tag == "Player2")
+        if (collider.gameObject.tag == "Player1" && player1Counter == 0)
         {
-            counter++;
-            if (counter == 1)
-            {
-                Debug.Log("asdf");
-                //SceneManager.LoadScene("Level 1 - Push box 1");
-            }
+            player1Counter++;
+        }
+        if (collider.gameObject.tag == "Player2" && player2Counter == 0)
+        {
+            player2Counter++;
+        }
+        if (player1Counter == 1 && player2Counter == 1)
+        {
+            SceneManager.LoadScene(sceneName: nextSceneName);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player1" && player1Counter == 1)
+        {
+            player1Counter--;
+            Debug.Log("P1 exited");
+        }
+        if (collider.gameObject.tag == "Player2" && player2Counter == 1)
+        {
+            player2Counter--;
+            Debug.Log("P2 exited");
+
         }
     }
 }
