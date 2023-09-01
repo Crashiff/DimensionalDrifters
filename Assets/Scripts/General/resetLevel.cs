3256cda520560a3e71a572b7e52dc1636652f87f
Talbe <talbenzvi00@gmail.com>
2023-09-01 20:40:13 +0300

    WIP: game over screen

--- Assets/Scripts/General/resetLevel.cs
+++ Assets/Scripts/General/resetLevel.cs
@@ -2,17 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using TMPro;
 public class resetLevel : MonoBehaviour
 {
     public float mark = -10;
+    public GameObject gameOverImage;
+    public TextMeshProUGUI countDownText;
+    private int countDown;
+    bool isCoroutineRunning = false;
+
+
+    private void Start()
+    {
+        countDown = 5;
+    }
 
     void Update()
     {
-        if(this.transform.position.y < mark)
+        if (this.transform.position.y < mark)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            ActivateGameOverScreen();
+            if (!isCoroutineRunning)
+            {
+                StartCoroutine(SpawnTimer());
+
+            }
         }
 
     }
+
+    void ActivateGameOverScreen()
+    {
+        gameOverImage.SetActive(true);
+    }
+
+    private IEnumerator SpawnTimer()
+    {
+        isCoroutineRunning = true;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            countDown--;
+            if (countDown == 0)
+            {
+                DeactivateGameOverScreen();
+            }
+            countDownText.text = "Restarting in " + countDown + " seconds";
+        }
+    }
+
+
+        void DeactivateGameOverScreen()
+        {
+            gameOverImage.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 }
