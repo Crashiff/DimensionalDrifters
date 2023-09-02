69d96e7d67bb34ad5928de9d67dec3d04f9a08aa
Natanel <natanel.shiff@gmail.com>
2023-09-03 01:16:31 +0300

    Pause menu working in all levels as prefab

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+
+    public GameObject PauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() 
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+}
