ad371288b484020605fc425c8aacacf1edbd4bbf
Talbe <talbenzvi00@gmail.com>
2023-09-03 22:42:30 +0300

    WIP: color change level. blocks gone after success

--- Assets/Scripts/2D_Level/CheckColors.cs
+++ Assets/Scripts/2D_Level/CheckColors.cs
@@ -25,11 +25,21 @@ public class CheckColors : MonoBehaviour
         if (platform1_color == 2 && platform2_color == 1 && platform3_color == 2 && platform4_color == 1 && platform5_color == 2)
         {
             Debug.Log("yay1");
+            GameObject[] colorBlocks = GameObject.FindGameObjectsWithTag("colorRow1");
+            foreach (GameObject obj in colorBlocks)
+            {
+                obj.SetActive(false);
+            }
         }
 
         else if (platform1_color == 2 && platform2_color == 1 && platform3_color == 0 && platform4_color == 2 && platform5_color == 1)
         {
             Debug.Log("yay2");
+            GameObject[] colorBlocks = GameObject.FindGameObjectsWithTag("colorRow2");
+            foreach (GameObject obj in colorBlocks)
+            {
+                obj.SetActive(false);
+            }
         }
 
     }
