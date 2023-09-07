74dc8b68bd5375a18a00a43e8c2b1fcf79efb2e8
Natanel <natanel.shiff@gmail.com>
2023-09-07 21:57:15 +0300

    Change overall shader working

--- Assets/Settings.cs
+++ Assets/Settings.cs
@@ -8,22 +8,30 @@ public class Settings : MonoBehaviour
 
     public static void SwitchMaterials()
     {
-        useFolder1 = !useFolder1;
-
-        // Find all objects in the scene and update their materials
-        Renderer[] renderers = FindObjectsOfType<Renderer>();
-        foreach (Renderer renderer in renderers)
+    useFolder1 = !useFolder1;
+    Renderer[] renderers = FindObjectsOfType<Renderer>();
+    foreach (Renderer renderer in renderers)
+    {
+        if (renderer == null || renderer.sharedMaterial == null)
         {
-            if (useFolder1)
-            {
-                Debug.Log("Assets/Materials/Standard/" + renderer.sharedMaterial.name + ".mat");
-                renderer.material = Resources.Load<Material>("Materials/Standard/" + renderer.sharedMaterial.name);
-            }
-            else
-            {
-                Debug.Log("Assets/Materials/Cartoon/" + renderer.sharedMaterial.name + ".mat");
-                renderer.material = Resources.Load<Material>("Materials/Cartoon/" + renderer.sharedMaterial.name);
-            }
+            continue; 
+        }
+
+        if (!(renderer.gameObject.name == "Element3D"))
+        {
+            continue; 
+        }
+
+        if (useFolder1)
+        {
+            Debug.Log("Assets/Materials/Standard/" + renderer.sharedMaterial.name + ".mat");
+            renderer.material = Resources.Load<Material>("Materials/Standard/" + renderer.sharedMaterial.name);
+        }
+        else
+        {
+            Debug.Log("Assets/Materials/Cartoon/" + renderer.sharedMaterial.name + ".mat");
+            renderer.material = Resources.Load<Material>("Materials/Cartoon/" + renderer.sharedMaterial.name);
         }
     }
+}
 }
