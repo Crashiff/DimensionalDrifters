f8eb4b86f0ab8e254e23602a885852673c9fa0d6
Natanel <natanel.shiff@gmail.com>
2023-09-12 02:21:37 +0300

    YAYYYYYYYYYYYYY 3!!!!!!!!!!!!! Final

--- Assets/Scripts/General/PauseMenu.cs
+++ Assets/Scripts/General/PauseMenu.cs
@@ -18,7 +18,7 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Q))
         {
             if (IsPaused) {
                 Resume();
